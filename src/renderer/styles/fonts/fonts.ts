710590374a40550edd67e27e13ee1538084cc5f0
FairyScript <h@queb.fun>
2021-03-30 06:21:38 +0800

    font xiaowei

--- src/renderer/styles/fonts/fonts.ts
+++ src/renderer/styles/fonts/fonts.ts
@@ -1,10 +1,20 @@
 import { css } from "@emotion/react";
 import SourceHanFont from './SourceHanSansCN-Regular.otf'
+import XiaoWei from './站酷小薇LOGO体.otf'
+
 
 export const sourceHan = css`
   font-family: 'SourceHanSans', sans-serif;
   @font-face {
     font-family: 'SourceHanSans';
     src: url(${SourceHanFont});
+  }
+`
+
+export const xiaoWeiLogo = css`
+  font-family: 'xiaoWeiLogo', sans-serif;
+  @font-face {
+    font-family: 'xiaoWeiLogo';
+    src: url(${XiaoWei});
   }
 `
